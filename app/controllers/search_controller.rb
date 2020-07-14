b0b3595e843ecabbb07f14a0003376a3c723b119
Ross Perry <perryr16@gmail.com>
2020-07-14 09:27:38 -0600

    capybara can select nation

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,8 @@
+class SearchController < ApplicationController
+
+  def index
+    binding.pry
+  end
+  
+
+end
---
+++ spec/features/search_members_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Airbender API" do 
+
+  it "searrch returns members of a nation" do
+    visit "/"
+
+    select('Fire Nation', from: :nation)
+    click_on "Search For Members"
+
+    
+  end
+  
+
+end
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members"
+# Then I should be on page "/search"
+# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
+# And I should see a list with the detailed information for the 20 members of the Fire Nation.
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies and a list of enemies
+# - Any affiliation that the member has
